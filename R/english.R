aaa7fbd41bed6172be979578fbe8f03e79a8ad59
Bill Venables <Bill.Venables@gmail.com>
2018-05-19 05:52:56 +0000

    version 1.1-3

--- R/english.R
+++ R/english.R
@@ -33,6 +33,8 @@ as.numeric.english <- function(x, ...) {
 }
 
 print.english <- function (x, ...) {
+  # opts <- options(scipen = 100)
+  # on.exit(options(opts))
   print(noquote(as.character.english(x)))
   invisible(x)
 }
@@ -41,84 +43,93 @@ sort.english <- function (x, decreasing = FALSE, ...) {
   structure(NextMethod("sort"), class = "english", useUK = attr(x, "useUK"))
 }
 
-as.character.english <- local({
-  ones <- structure(c("", "one", "two", "three", "four", "five", "six",
-                      "seven", "eight", "nine"),
-                  .Names = c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"))
-  suffixes <- c("thousand", "million", "billion", "trillion", "quadrillion",
-                "quintillion", "sextillion", "septillion")
-  teens <- structure(c("ten", "eleven", "twelve", "thirteen", "fourteen",
-                       "fifteen", "sixteen", "seventeen", "eighteen",
-                       "nineteen"),
-                     .Names = c("0", "1", "2", "3", "4", "5", "6",
-                                "7", "8", "9"))
-  tens <- structure(c("twenty", "thirty", "forty", "fifty", "sixty",
-                      "seventy", "eighty", "ninety"),
-                    .Names = c("2", "3", "4", "5", "6", "7", "8", "9"))
+ones <- c(`0` = "", `1` = "one", `2` = "two", `3` = "three", `4` = "four",
+          `5` = "five", `6` = "six", `7` = "seven", `8` = "eight", `9` = "nine")
 
-  makeNumber <- function (n)
-    as.numeric(paste(n, collapse = ""))
-  trim <- function (text)
-    sub("^ *", "", sub(" *$", "", gsub("  +", " ", text)))
-  
-  function (x, ...) {
-    UK <- attr(x, "useUK")
-    and <- function (dvec) {
-      if(UK && (d <- makeNumber(dvec)) > 0 && d < 100) "and" else ""
-    }
-    helper <- function (x) {
-      digits <- rev(strsplit(as.character(x), "")[[1]])
-      nDigits <- length(digits)
-      if (nDigits == 1)
-        as.vector(ones[digits])
-      else if (nDigits == 2)
-        if (x <= 19)
-          as.vector(teens[digits[1]])
-        else trim(paste(tens[digits[2]],
-                        helper(as.numeric(digits[1]))))
-      else if (nDigits == 3)
-        trim(paste(ones[digits[3]], "hundred",
-                   and(digits[2:1]),
-                   helper(makeNumber(digits[2:1]))))
-      else {
-        nSuffix <- ((nDigits + 2) %/% 3) - 1
-        if (nSuffix > length(suffixes)) {
-          warning(paste(x, "is too large!"))
-          return(as.character(as.vector(x)))
-        }
-        trim(paste(helper(makeNumber(digits[nDigits:(3 * nSuffix + 1)])),
-                   suffixes[nSuffix],
-                   and(digits[(3 * nSuffix):1]),
-                   helper(makeNumber(digits[(3 * nSuffix):1]))))
-      }
-    }
-    
-    opts <- options(scipen = 100)
-    on.exit(options(opts))
-    r <- character(length(x))
-    bad <- is.na(x) | is.nan(x) | is.infinite(x)
-    if (any(!bad & x%%1 != 0)) {
-      warning("non-integer values rounded for display")
-      x <- round(x)
-    }
-    if (any(n <- !bad & x < 0))
-      r[n] <- paste("minus", sapply(-x[n], helper))
-    if (any(z <- !bad & x == 0))
-      r[z] <- "zero"
-    if (any(p <- !bad & x > 0))
-      r[p] <- sapply(x[p], helper)
-    r[is.na(x)] <- ""
-    r[is.nan(x)] <- "not a number"
-    if (any(k <- x < 0 & is.infinite(x)))
-      r[k] <- "minus infinity"
-    if (any(k <- x > 0 & is.infinite(x)))
-      r[k] <- "infinity"
-    names(r) <- names(x)
-    r
+suffixes <- c("thousand", "million", "billion", "trillion", "quadrillion",
+              "quintillion", "sextillion", "septillion")
+
+teens <- c(`0` = "ten", `1` = "eleven", `2` = "twelve", `3` = "thirteen",
+           `4` = "fourteen", `5` = "fifteen", `6` = "sixteen", `7` = "seventeen",
+           `8` = "eighteen", `9` = "nineteen")
+
+tens <- c(`2` = "twenty", `3` = "thirty", `4` = "forty", `5` = "fifty",
+          `6` = "sixty", `7` = "seventy", `8` = "eighty", `9` = "ninety")
+
+makeNumber <- function (n)
+  as.numeric(paste(n, collapse = ""))
+
+trim <- function (text)
+  sub("^ *", "", sub(" *$", "", gsub("  +", " ", text)))
+
+split_digits <- function(x) {
+  d <- character()
+  while(x > 0) {
+    d <- c(d, x %% 10)
+    x <- x %/% 10
   }
-})
+  if(length(d) == 0)
+    d <- "0"
+  d
+}
 
-rep.english <- function (x, ...) 
+as.character.english <- function (x, ...) {
+  # opts <- options(scipen = 100)
+  # on.exit(options(opts))
+  UK <- attr(x, "useUK")
+  and <- function (dvec) {
+    if(UK && (d <- makeNumber(dvec)) > 0 && d < 100) "and" else ""
+  }
+  helper <- function (x) {
+    # digits <- rev(strsplit(as.character(x), "")[[1]])
+    digits <- split_digits(x)
+    nDigits <- length(digits)
+    if (nDigits == 1)
+      as.vector(ones[digits])
+    else if (nDigits == 2)
+      if (x <= 19)
+        as.vector(teens[digits[1]])
+    else trim(paste(tens[digits[2]],
+                    helper(as.numeric(digits[1]))))
+    else if (nDigits == 3)
+      trim(paste(ones[digits[3]], "hundred",
+                 and(digits[2:1]),
+                 helper(makeNumber(digits[2:1]))))
+    else {
+      nSuffix <- ((nDigits + 2) %/% 3) - 1
+      if (nSuffix > length(suffixes)) {
+        warning(paste(x, "is too large!"))
+        return(as.character(as.vector(x)))
+      }
+      trim(paste(helper(makeNumber(digits[nDigits:(3 * nSuffix + 1)])),
+                 suffixes[nSuffix],
+                 and(digits[(3 * nSuffix):1]),
+                 helper(makeNumber(digits[(3 * nSuffix):1]))))
+    }
+  }
+  r <- character(length(x))
+  bad <- is.na(x) | is.nan(x) | is.infinite(x)
+  if (any(!bad & x%%1 != 0)) {
+    warning("non-integer values rounded for display")
+    x <- round(x)
+  }
+  if (any(n <- !bad & x < 0))
+    r[n] <- paste("minus", sapply(-x[n], helper))
+  if (any(z <- !bad & x == 0))
+    r[z] <- "zero"
+  if (any(p <- !bad & x > 0))
+    r[p] <- sapply(x[p], helper)
+  r[is.na(x)] <- ""
+  r[is.nan(x)] <- "not a number"
+  if (any(k <- x < 0 & is.infinite(x)))
+    r[k] <- "minus infinity"
+  if (any(k <- x > 0 & is.infinite(x)))
+    r[k] <- "infinity"
+  names(r) <- names(x)
+  r
+}
+
+rep.english <- function (x, ...)
     structure(rep(unclass(x), ...), class = class(x))
 
 `[.english` <- function(x, i) {
